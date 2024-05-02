4826c50fbe6004c6d8d9de77b8baced12b3e55b2
rvcas <x@rvcas.dev>
2024-05-01 22:54:38 -0400

    feat(list): add --installed flag to show local bins

--- src/cmd/list.rs
+++ src/cmd/list.rs
@@ -1,24 +1,47 @@
 use miette::IntoDiagnostic;
 
-/// List aiken versions
+use crate::utils::root_dir;
+
+/// List available aiken versions
 #[derive(clap::Args)]
-pub struct Args {}
+pub struct Args {
+    /// show installed versions
+    #[clap(short, long, default_value = "false")]
+    installed: bool,
+}
 
-pub async fn exec(_args: Args) -> miette::Result<()> {
-    let octocrab = octocrab::instance();
+pub async fn exec(args: Args) -> miette::Result<()> {
+    if args.installed {
+        let aiken_root = root_dir()?;
+        let versions_dir = aiken_root.join("versions");
 
-    let releases = octocrab
-        .repos("aiken-lang", "aiken")
-        .releases()
-        .list()
-        .send()
-        .await
-        .into_diagnostic()?;
+        let mut entries = tokio::fs::read_dir(&versions_dir).await.into_diagnostic()?;
 
-    println!("aikup: available versions");
+        println!("aikup: installed versions");
 
-    for release in releases {
-        println!("{}", release.tag_name);
+        while let Some(entry) = entries.next_entry().await.into_diagnostic()? {
+            let path = entry.path();
+
+            if path.is_dir() {
+                println!("{}", path.file_name().unwrap().to_string_lossy());
+            }
+        }
+    } else {
+        let octocrab = octocrab::instance();
+
+        let releases = octocrab
+            .repos("aiken-lang", "aiken")
+            .releases()
+            .list()
+            .send()
+            .await
+            .into_diagnostic()?;
+
+        println!("aikup: available versions");
+
+        for release in releases {
+            println!("{}", release.tag_name);
+        }
     }
 
     Ok(())
